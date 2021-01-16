e78cadb13eabe78b382e58314ee974e6556d7ab7
derek <derekle.creative@gmail.com>
2021-01-16 17:37:39 -0600

    commit tree 032acb41701bb10404e22c0823fadd939fe1ab21

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
 def square_array(array)
-  # your code here
+  tempArray = []
+  tempArray.clear
+  array.each do |x|
+    y = x**2
+    tempArray << y
+  end
+  return tempArray
 end
+
+numbers = [1,2,3]
+square_array(numbers)
+
+newNum =[10,20,30]
+square_array(newNum)
+
+def collect_array(array)
+  array = array.collect{|x| x**2}
+end
+
+collect_array(numbers)
+collect_array(newNum)
